1573b64b2efb155fe58a2bec2a9ba465ce345edc
rinat <>
2018-08-14 16:02:54 +0500

    add apollo-express add express

--- app/index.ts
+++ app/index.ts
@@ -1,35 +1,50 @@
-import { ApolloServer, defaultPlaygroundOptions } from 'apollo-server'
-import debug from 'debug'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
 import { makeExecutableSchema } from 'graphql-tools'
 import { Prisma } from 'prisma-binding'
+import { Prisma as PrismaType } from 'app/graphql/generated/prisma'
 import { resolvers, typeDefs } from 'app/graphql'
-
-const log = debug('app')
+import cookieParser from 'cookie-parser'
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+const app = express()
+
+const prisma = <PrismaType><any>new Prisma({
+  typeDefs: 'app/graphql/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+})
+
+app.use(cookieParser())
+
+app.use((req: any, res, next) => {
+
+  prisma.query
+    .user({ where: { email: req.query.currentUser || req.cookies.currentUser || '' } }, '{id}')
+    .then((result) => {
+
+      if (result) {
+        req.headers['x-authenticated-user-id'] = result.id
+        next()
+      }
+      else {
+        res.status(401).set('Content-Type', 'text/plain').end('Unauthorized. Set currentUser')
+      }
+    }, next)
+})
+
 const server = new ApolloServer({
   schema,
-  context() {
+  context({ req }: any) {
     return {
-      user: {
-        id: 'cjkhv9pqg0hhf0a168cnsreq6',
-        email: 'gabriel@frank.ly',
-      },
-      prisma: new Prisma({
-        typeDefs: 'app/graphql/generated/prisma.graphql',
-        endpoint: process.env.PRISMA_ENDPOINT,
-      }),
+      user: { id: req.headers['x-authenticated-user-id'] },
+      prisma,
     }
-  },
-  playground: {
-    settings: {
-      ...defaultPlaygroundOptions.settings,
-      ['request.credentials']: 'include',
-    },
   },
 })
 
-server.listen(process.env.PORT).then(({ url }) => {
-  log(`Listening at ${url}`)
-})
+server.applyMiddleware({ app }) // app is from an existing express app
+
+app.listen({ port: process.env.PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`),
+)
