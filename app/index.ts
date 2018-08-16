42e73e78000c12e296669d22ab2dd3d9a17f42e6
rinat <>
2018-08-16 19:33:41 +0500

    clear ApolloServer (index.ts)

--- app/index.ts
+++ app/index.ts
@@ -1,44 +1,9 @@
-import { ApolloServer } from 'apollo-server-express'
-import express from 'express'
+import { ApolloServer } from 'apollo-server'
 import { makeExecutableSchema } from 'graphql-tools'
 import { Prisma } from 'prisma-binding'
-import { Prisma as PrismaType } from 'app/graphql/generated/prisma'
 import { resolvers, typeDefs } from 'app/graphql'
-import cookieParser from 'cookie-parser'
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
-
-const app = express()
-
-app.use(cookieParser())
-
-app.use(async (req, res, next) => {
-  try {
-    req.headers['x-authenticated-user-id'] = ''
-
-    const email =
-      req.query.currentUser ||
-      req.headers['x-current-user'] ||
-      req.cookies.currentUser
-
-    if (email) {
-      const prisma = <PrismaType>(<any>new Prisma({
-        typeDefs: 'app/graphql/generated/prisma.graphql',
-        endpoint: process.env.PRISMA_ENDPOINT,
-      }))
-
-      const user = await prisma.query.user({ where: { email } }, `{ id }`)
-
-      if (user && user.id) {
-        req.headers['x-authenticated-user-id'] = user.id
-      }
-    }
-
-    next()
-  } catch (exc) {
-    next(exc)
-  }
-})
 
 const server = new ApolloServer({
   schema,
@@ -53,13 +18,6 @@ const server = new ApolloServer({
   },
 })
 
-server.applyMiddleware({ app, path: '/' }) // app is from an existing express app
-
-app.listen({ port: process.env.PORT }, () =>
-  // tslint:disable-next-line:no-console
-  console.log(
-    `🚀 Server ready at http://localhost:${process.env.PORT}${
-      server.graphqlPath
-    }`
-  )
-)
+server.listen(process.env.PORT).then(({ url }) => {
+  console.log(`🚀 Apollo server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+})
