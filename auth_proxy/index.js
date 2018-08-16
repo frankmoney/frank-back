75d20a72fdce850b988b7a195b87cbac115ea4a3
rinat <>
2018-08-16 19:35:37 +0500

    add auth proxy app

---
+++ auth_proxy/index.js
@@ -0,0 +1,53 @@
+import { Prisma } from 'prisma-binding'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import proxy from 'http-proxy-middleware'
+
+const PORT = process.env.PORT || 3000
+const APOLLO_PORT = process.env.APOLLO_PORT || 3001
+const PRISMA_ENDPOINT = process.env.PRISMA_ENDPOINT || 'http://prisma.frank-dev1.frank.ly'
+
+const app = express()
+
+app.use(cookieParser())
+
+app.use(async (req, res, next) => {
+  try {
+    req.currentUserId = ''
+
+    const email =
+      req.query.currentUser ||
+      req.headers['x-current-user'] ||
+      req.cookies.currentUser
+
+    if (email) {
+      const prisma = (
+        new Prisma({
+          typeDefs: '../app/graphql/generated/prisma.graphql',
+          endpoint: PRISMA_ENDPOINT,
+        }))
+
+      const user = await prisma.query.user({ where: { email } }, `{id}`)
+
+      if (user && user.id) {
+        req.currentUserId = user.id
+      }
+    }
+
+    next()
+  } catch (exc) {
+    next(exc)
+  }
+})
+
+
+app.use(proxy('/', {
+  target: `http://localhost:${APOLLO_PORT}`,
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    proxyReq.setHeader('X-Authenticated-User-Id', req.currentUserId)
+  },
+}))
+
+
+app.listen(PORT, () => console.log(`Auth proxy listening on port ${PORT}`))
