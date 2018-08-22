86d87d3e82998b3d1ee6e063980f99f437249120
rinat <>
2018-08-22 13:37:00 +0500

    add updateStory mutation

--- app/graphql/stories/updateStory.ts
+++ app/graphql/stories/updateStory.ts
@@ -2,7 +2,7 @@ import { throwNotFound } from 'app/errors/NotFoundError'
 import createPrivateResolver from 'utils/createPrivateResolver'
 
 export default createPrivateResolver(
-  'deleteStory',
+  'updateStory',
   async ({ assert, args, prisma: { query, mutation }, info }) => {
 
     const accountId = args.accountId
@@ -29,8 +29,13 @@ export default createPrivateResolver(
 
     // await assert.accountAccess(accountId)  ..later
 
-    return mutation.deleteStory({
+    return mutation.updateStory({
       where: { id: storyId },
+      data: {
+        title: args.title,
+        body: args.body && JSON.parse(args.body),
+        coverImage: args.coverImage && JSON.parse(args.coverImage),
+      },
     }, info)
   },
 )
