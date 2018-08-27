0f904dcc8cff8af3037bacaf9c5bec6773c9a570
rinat <>
2018-08-27 17:33:50 +0300

    Connect payments to stories

--- app/graphql/stories/updateStory.ts
+++ app/graphql/stories/updateStory.ts
@@ -1,5 +1,6 @@
 import { throwNotFound } from 'app/errors/NotFoundError'
 import createPrivateResolver from 'utils/createPrivateResolver'
+import _ from 'lodash'
 
 export default createPrivateResolver(
   'updateStory',
@@ -7,7 +8,7 @@ export default createPrivateResolver(
     const accountId = args.accountId
     const storyId = args.storyId
 
-    const story = (await query.stories(
+    const story: any = (await query.stories(
       {
         where: {
           AND: [
@@ -22,7 +23,7 @@ export default createPrivateResolver(
           ],
         },
       },
-      '{ id }'
+      '{ id, payments { id } }',
     ))[0]
 
     if (!story) {
@@ -31,16 +32,30 @@ export default createPrivateResolver(
 
     // await assert.accountAccess(accountId)  ..later
 
+    const data: any = {
+      title: args.title,
+      body: args.body && JSON.parse(args.body),
+      coverImage: args.coverImage && JSON.parse(args.coverImage),
+    }
+
+    if (args.paymentsIds) {
+
+      const currentPaymentsIds = _.map(story.payments, (payment) => payment.id)
+      const toConnectPaymentsIds = _.difference(args.paymentsIds, currentPaymentsIds)
+      const toDisconnectPaymentsIds = _.difference(currentPaymentsIds, args.paymentsIds)
+
+      data.payments = {
+        connect: _.map(toConnectPaymentsIds, (id) => ({ id })),
+        disconnect: _.map(toDisconnectPaymentsIds, (id) => ({ id })),
+      }
+    }
+
     return mutation.updateStory(
       {
         where: { id: storyId },
-        data: {
-          title: args.title,
-          body: args.body && JSON.parse(args.body),
-          coverImage: args.coverImage && JSON.parse(args.coverImage),
-        },
+        data,
       },
-      info
+      info,
     )
-  }
+  },
 )
