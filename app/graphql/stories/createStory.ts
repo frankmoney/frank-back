ac8cdf535c9bb6cd573cd6ec1059065e635adfa2
rinat <>
2018-08-28 11:13:20 +0300

    Update createStory muttion

--- app/graphql/stories/createStory.ts
+++ app/graphql/stories/createStory.ts
@@ -1,20 +1,29 @@
 import createPrivateResolver from 'utils/createPrivateResolver'
+import _ from 'lodash'
 
 export default createPrivateResolver(
   'createStory',
   ({ assert, args, prisma: { mutation }, info }) => {
+
     // await assert.accountAccess(accountId)  ..later
 
+    const data: any = {
+      title: args.title,
+      body: JSON.parse(args.body),
+      coverImage: JSON.parse(args.coverImage),
+      account: { connect: { id: args.accountId } },
+    }
+
+    if (args.paymentsIds) {
+
+      data.payments = {
+        connect: _.map(args.paymentsIds, (id) => ({ id })),
+      }
+    }
+
     return mutation.createStory(
-      {
-        data: {
-          title: args.title,
-          body: JSON.parse(args.body),
-          coverImage: JSON.parse(args.coverImage),
-          account: { connect: { id: args.accountId } },
-        },
-      },
-      info
+      { data },
+      info,
     )
-  }
+  },
 )
