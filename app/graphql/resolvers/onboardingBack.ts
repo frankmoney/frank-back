0f509cfd5543b412fa534030a05e509f6216a8b5
rinat <>
2018-09-19 16:40:36 +0500

    onboarding: update back mutation

--- app/graphql/resolvers/onboardingBack.ts
+++ app/graphql/resolvers/onboardingBack.ts
@@ -1,10 +1,11 @@
 import { throwArgumentError } from 'app/errors/ArgumentError'
+import { OnboardingUpdateInput } from 'app/graphql/generated/prisma'
 import OnboardingType from 'app/graphql/schema/OnboardingType/OnboardingType'
 import {
   ACCOUNT_STEP,
   ACCOUNTS_STEP,
   CATEGORIES_STEP,
-  CREDENTIALS_STEP,
+  CREDENTIALS_STEP, MFA_STEP,
 } from 'app/onboarding/constants'
 import findExistingOnboarding from 'app/onboarding/findExistingOnboarding'
 import createMutations from 'utils/createMutations'
@@ -20,15 +21,30 @@ const onboardingBack = createPrivateResolver(
       return throwArgumentError()
     }
 
-    const steps = [CREDENTIALS_STEP, ACCOUNTS_STEP, ACCOUNT_STEP, CATEGORIES_STEP]
+    let newStep = CREDENTIALS_STEP
 
-    const step = steps[steps.indexOf(existingOnboarding.step) - 1] || CREDENTIALS_STEP
+    switch (existingOnboarding.step) {
+      case CATEGORIES_STEP:
+        newStep = ACCOUNT_STEP
+        break
+      case ACCOUNT_STEP:
+        newStep = ACCOUNTS_STEP
+        break
+      case ACCOUNTS_STEP:
+      case MFA_STEP:
+        newStep = CREDENTIALS_STEP
+        break
+    }
+
+    const data: OnboardingUpdateInput = { step: newStep }
+
+    if (newStep === CREDENTIALS_STEP) {
+      data.member = { disconnect: true }
+    }
 
     return await prisma.mutation.updateOnboarding({
       where: { id: existingOnboarding.id },
-      data: {
-        step,
-      },
+      data,
     })
   },
 )
