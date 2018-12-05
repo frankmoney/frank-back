a6504eb5b35aeda6b98c7c3d81cc8009036b6fbd
Ilya "aq" Kozlov <upsnake@gmail.com>
2018-12-05 07:09:40 +0300

    pie chart redone

--- packages/frank-back-api-gql/src/api/schema/LedgerPieChartItemType.ts
+++ packages/frank-back-api-gql/src/api/schema/LedgerPieChartItemType.ts
@@ -4,7 +4,6 @@ import CategoryType from './CategoryType'
 const LedgerPieChartItemType = Type('LedgerPieChartItem', type =>
   type.fields(field => ({
     category: field.ofType(CategoryType).nullable(),
-    revenue: field.ofFloat(),
     spending: field.ofFloat(),
   }))
 )
