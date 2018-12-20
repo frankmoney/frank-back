44922ee4ee19e458f0f9606548bdc74de1a9a9fd
Ilya "aq" Kozlov <upsnake@gmail.com>
2018-12-20 21:47:08 +0300

    /http/sign-up - handle exception

--- packages/frank-back-api-gql/src/api/http/handlers/handleSignUp.ts
+++ packages/frank-back-api-gql/src/api/http/handlers/handleSignUp.ts
@@ -35,130 +35,139 @@ const handleSignUp = async (
 ) => {
   const log = scope.logFor('http:signUp')
 
-  if (ctx.is('json')) {
-    const respondWithInvalidArgument = (field?: string) => {
-      ctx.response.status = 401
-      ctx.response.body = {
-        code: 'invalid_argument',
-        field,
+  try {
+    if (ctx.is('json')) {
+      const respondWithInvalidArgument = (field?: string) => {
+        ctx.response.status = 401
+        ctx.response.body = {
+          code: 'invalid_argument',
+          field,
+        }
       }
-    }
 
-    const body: Body = await parseBody.json(ctx)
+      const body: Body = await parseBody.json(ctx)
 
-    if (!body) {
-      return respondWithInvalidArgument()
-    }
-
-    if (!body.team) {
-      return respondWithInvalidArgument('team')
-    }
-
-    if (!body.user) {
-      return respondWithInvalidArgument('user')
-    }
-
-    if (nullOrEmpty(body.team.name)) {
-      return respondWithInvalidArgument('team.name')
-    }
-
-    if (nullOrEmpty(body.user.email)) {
-      return respondWithInvalidArgument('user.email')
-    }
-
-    if (nullOrEmpty(body.user.password)) {
-      return respondWithInvalidArgument('user.password')
-    }
-
-    if (nullOrEmpty(body.user.firstName)) {
-      return respondWithInvalidArgument('user.firstName')
-    }
-
-    const name = body.team.name.trim()
-    const city = normalize(body.team.city)
-    const size = normalize(body.team.size)
-
-    const email = body.user.email.trim()
-    const lastName = normalize(body.user.lastName)
-    const firstName = body.user.firstName.trim()
-    const phone = normalize(body.user.phone)
-
-    const passwordHash = hashPassword(body.user.password)
-
-    const color =
-      config.USER_COLORS[Math.floor(Math.random() * config.USER_COLORS.length)]
-
-    const teamId = await createTeam(
-      {
-        name,
-        city,
-        size,
-      },
-      scope
-    )
-
-    const { status, userId } = await createPersonUserMaybe(
-      {
-        email,
-        passwordHash,
-        lastName,
-        firstName,
-        color,
-        phone,
-      },
-      scope
-    )
-
-    if (status === 'duplicate') {
-      ctx.response.status = 409
-      ctx.response.body = {
-        code: 'email_in_use',
+      if (!body) {
+        return respondWithInvalidArgument()
       }
-      return
-    }
 
-    await createTeamMember(
-      {
-        teamId,
-        userId,
-        roleId: TeamMemberRole.administrator,
-      },
-      scope
-    )
+      if (!body.team) {
+        return respondWithInvalidArgument('team')
+      }
 
-    await scope.uow.commit()
+      if (!body.user) {
+        return respondWithInvalidArgument('user')
+      }
 
-    try {
-      const mail = userCreationConfirmationMail({
-        data: {
-          user: {
-            lastName,
-            firstName,
-          },
-          link: config.MAIL.links.userCreationConfirmation({
-            token: userId,
-          }),
+      if (nullOrEmpty(body.team.name)) {
+        return respondWithInvalidArgument('team.name')
+      }
+
+      if (nullOrEmpty(body.user.email)) {
+        return respondWithInvalidArgument('user.email')
+      }
+
+      if (nullOrEmpty(body.user.password)) {
+        return respondWithInvalidArgument('user.password')
+      }
+
+      if (nullOrEmpty(body.user.firstName)) {
+        return respondWithInvalidArgument('user.firstName')
+      }
+
+      const name = body.team.name.trim()
+      const city = normalize(body.team.city)
+      const size = normalize(body.team.size)
+
+      const email = body.user.email.trim()
+      const lastName = normalize(body.user.lastName)
+      const firstName = body.user.firstName.trim()
+      const phone = normalize(body.user.phone)
+
+      const passwordHash = hashPassword(body.user.password)
+
+      const color =
+        config.USER_COLORS[Math.floor(Math.random() * config.USER_COLORS.length)]
+
+      const teamId = await createTeam(
+        {
+          name,
+          city,
+          size,
         },
-      })
-
-      await scope.mailer.send({ to: email }, mail)
-
-      log.info(`Sent user creation confirmation mail to "${email}"`)
-    } catch (exc) {
-      log.error(
-        exc,
-        `Failed to send user creation confirmation mail to "${email}"`
+        scope
       )
-    }
 
-    ctx.response.status = 201
-    ctx.response.body = {
-      token: body.user.email.trim().toLowerCase(),
+      const { status, userId } = await createPersonUserMaybe(
+        {
+          email,
+          passwordHash,
+          lastName,
+          firstName,
+          color,
+          phone,
+        },
+        scope
+      )
+
+      if (status === 'duplicate') {
+        ctx.response.status = 409
+        ctx.response.body = {
+          code: 'email_in_use',
+        }
+        return
+      }
+
+      await createTeamMember(
+        {
+          teamId,
+          userId,
+          roleId: TeamMemberRole.administrator,
+        },
+        scope
+      )
+
+      await scope.uow.commit()
+
+      try {
+        const mail = userCreationConfirmationMail({
+          data: {
+            user: {
+              lastName,
+              firstName,
+            },
+            link: config.MAIL.links.userCreationConfirmation({
+              token: userId,
+            }),
+          },
+        })
+
+        await scope.mailer.send({ to: email }, mail)
+
+        log.info(`Sent user creation confirmation mail to "${email}"`)
+      } catch (exc) {
+        log.error(
+          exc,
+          `Failed to send user creation confirmation mail to "${email}"`
+        )
+      }
+
+      ctx.response.status = 201
+      ctx.response.body = {
+        token: body.user.email.trim().toLowerCase(),
+      }
+    } else {
+      ctx.response.status = 415
+      ctx.response.body = {
+        code: 'invalid_body_type',
+      }
     }
-  } else {
-    ctx.response.status = 415
+  } catch (exc) {
+    log.error(exc)
+
+    ctx.response.status = 500
     ctx.response.body = {
-      code: 'invalid_body_type',
+      code: 'internal_error',
     }
   }
 }
