973d3adef6b5c69ce9157b56be3d5cf87a9bbb6d
rinat <>
2018-12-27 21:01:06 +0500

    update mailer helper

--- packages/frank-back-api-gql/src/mailer/helpers.ts
+++ packages/frank-back-api-gql/src/mailer/helpers.ts
@@ -1,23 +1,33 @@
 import Payment from 'store/types/Payment'
-import { format, isSameYear } from 'date-fns'
+import { format, isSameYear, isSameMonth } from 'date-fns'
+
+const MONTH_YEAR_TEMPLATE = 'MMM, YYYY'
 
 export const paymentsDatesDescription = (sortedPayments: Payment[]) => {
-
   if (sortedPayments.length === 0) {
     return ''
   }
 
   if (sortedPayments.length === 1) {
-    return format(sortedPayments[0].postedOn, 'MMM, YYYY')
+    return format(sortedPayments[0].postedOn, MONTH_YEAR_TEMPLATE)
   }
 
   const firstDate = sortedPayments[0].postedOn
   const lastDate = sortedPayments[sortedPayments.length - 1].postedOn
 
-  if (isSameYear(firstDate, lastDate)) {
-
-    return `${format(firstDate, 'MMM')} - ${format(lastDate, 'MMM, YYYY')}`
+  if (!isSameYear(firstDate, lastDate)) {
+    return `${format(firstDate, MONTH_YEAR_TEMPLATE)} - ${format(
+      lastDate,
+      MONTH_YEAR_TEMPLATE
+    )}`
   }
 
-  return `${format(firstDate, 'MMM, YYYY')} - ${format(lastDate, 'MMM, YYYY')}`
+  if (!isSameMonth(firstDate, lastDate)) {
+    return `${format(firstDate, 'MMM')} - ${format(
+      lastDate,
+      MONTH_YEAR_TEMPLATE
+    )}`
+  }
+
+  return format(firstDate, MONTH_YEAR_TEMPLATE)
 }
